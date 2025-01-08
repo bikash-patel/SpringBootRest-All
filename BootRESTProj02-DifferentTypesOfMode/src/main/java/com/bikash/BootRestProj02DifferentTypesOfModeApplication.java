b2aeb9db8fb9b654ad1647a7336a5ee62691397c
Bikash Patel <bikashpatel506@gmail.com>
2025-01-08 12:34:33 +0530

    Added two more proj initially

---
+++ BootRESTProj02-DifferentTypesOfMode/src/main/java/com/bikash/BootRestProj02DifferentTypesOfModeApplication.java
@@ -0,0 +1,13 @@
+package com.bikash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestProj02DifferentTypesOfModeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestProj02DifferentTypesOfModeApplication.class, args);
+	}
+
+}
---
+++ BootRESTProj02-DifferentTypesOfMode/src/test/java/com/bikash/BootRestProj02DifferentTypesOfModeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestProj02DifferentTypesOfModeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
